27fcb16071cb24a3edd06624a1513a26f32617a8
ablecan1-cmd <ablecan1@gmail.com>
2026-03-28 09:16:56 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import OlleShot from './OlleShot'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <OlleShot />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(OlleShot, null)
+  )
 )
-```
