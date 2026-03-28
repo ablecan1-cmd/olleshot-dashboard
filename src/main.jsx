5edc069256827c459871954da50feb98634c2634
ablecan1-cmd <ablecan1@gmail.com>
2026-03-28 09:08:58 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OlleShot from './OlleShot'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OlleShot />
+  </React.StrictMode>
+)
+```
